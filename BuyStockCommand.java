2b32989d5d93f3fd5f1ba0dca3ef8ba51fe841b6
terric <terric@email.sc.edu>
2020-10-08 18:33:36 -0400

    Second commit

--- BuyStockCommand.java
+++ BuyStockCommand.java
@@ -1,4 +1,14 @@
 
 public class BuyStockCommand {
-
+	private stockTrade StockTrade;
+	private String stockName;
+	private int shares;
+	
+	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares ) {
+		
+	}
+	
+	public void execute() {
+		
+	}
 }
