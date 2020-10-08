633925d5ef20b441db9057c3c3425308b0dc7ee2
Jonathan Terry <jonathanterry935@gmail.com>
2020-10-08 17:47:36 -0400

    Created Command interface

---
+++ Command.java
@@ -0,0 +1,9 @@
+/**
+ * This interface describes some methods used by various commands used in the stock market
+ * @author Jonathan Terry
+ *
+ */
+public interface Command {
+	public void execute();
+
+}
