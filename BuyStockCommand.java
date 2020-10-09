2c92b221c68db5de65d4e727da3916b769570f86
terric <terric@email.sc.edu>
2020-10-09 10:31:56 -0400

    Added in attribute definitions

--- BuyStockCommand.java
+++ BuyStockCommand.java
@@ -1,14 +1,16 @@
 
 public class BuyStockCommand {
-	private stockTrade StockTrade;
+	private StockTrade stockTrade;
 	private String stockName;
 	private int shares;
 	
 	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares ) {
-		
+		stockName = this.stockName;
+		shares = this.shares;
+		stockTrade = this.stockTrade;
 	}
 	
 	public void execute() {
-		
+		//needs to call buy command from StockTrade
 	}
 }
