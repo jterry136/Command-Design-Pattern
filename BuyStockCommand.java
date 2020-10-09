df3ceca2f04a021f4532dc96c1dc20f30bcf0547
terric <terric@email.sc.edu>
2020-10-09 11:14:42 -0400

    JavaDoc and method implimentation

--- BuyStockCommand.java
+++ BuyStockCommand.java
@@ -1,4 +1,9 @@
-
+/**
+ * This class will allow the user to buy a stock from the stock trade.
+ * @author terric
+ * @param <StockTrade>
+ * 
+ */
 public class BuyStockCommand {
 	private StockTrade stockTrade;
 	private String stockName;
@@ -9,8 +14,14 @@ public class BuyStockCommand {
 		shares = this.shares;
 		stockTrade = this.stockTrade;
 	}
-	
+	/**
+	 * This method will create a new instance of stock.
+	 * from there the buy method from StockTrade is called and passes in two parameters.
+	 * @param stockName: String value describing the name of a stock.
+	 * @param shares: integer value of the number of shares a user is buying.
+	 */
 	public void execute() {
-		//needs to call buy command from StockTrade
+		StockTrade stock = new StockTrade();
+		stock.buy(stockName, shares);
 	}
 }
